db7bd0b8ea5753d18900cb46362c58ed2adae859
Gianfi02 <gianfelice@outlook.com>
2025-11-28 12:07:18 +0100

    primo tentativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+    final Map<String, List<String>> shoppingList = {
+      'Fruttivendolo':['Mele','Pere','Banane','Ananas'],
+      'Panificio':['Panini','Pizza','Focaccia'],
+      'Macelleria':['Braciole','Salsiccie','Arrosticini']
+
+    };
+   MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: ListView.builder(
+          itemCount: shoppingList.length,
+          itemBuilder: (context, index){
+            String category = shoppingList.keys.elementAt(index);
+            List<String> items = shoppingList[category] ?? [];
+            int num = items.length;
+            return ListTile(
+              leading: Icon(Icons.accessible_forward_rounded),
+              title: Text(category),
+              subtitle: Text('items: $num'),
+              trailing: Icon(Icons.arrow_right_sharp),
+            );
+          },
         ),
       ),
     );
   }
 }
+
+
